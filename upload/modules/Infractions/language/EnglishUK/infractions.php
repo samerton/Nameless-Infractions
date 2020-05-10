fad1af5f1434a624b7f91ab88c3b43c520ac80a1
Sybsuper <36895604+Sybsuper@users.noreply.github.com>
2020-05-10 21:28:24 +0200

    Add view permission

--- upload/modules/Infractions/language/EnglishUK/infractions.php
+++ upload/modules/Infractions/language/EnglishUK/infractions.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *	Made by Samerton and Partydragen
+ *    Made by Samerton and Partydragen
  *  https://github.com/samerton/Nameless-Infractions
  *  NamelessMC version 2.0.0-pr6
  *
@@ -31,19 +31,20 @@ $language = array(
     'active' => 'Active',
     'no_infractions' => 'No infractions found.',
     'unknown' => 'Unknown',
-	
+    
     /*
      *  Staff Panel
      */
-	'infractions_settings' => 'Infractions Settings',
-	'link_location' => 'Link Location',
-	'plugin' => 'Plugin Integration',
-	'database_settings' => 'Update Database Settings',
-	'database_address' => 'Database Address',
-	'database_name' => 'Database Name',
-	'database_username' => 'Database Username',
-	'database_port' => 'Database Port',
-	'database_password' => 'Database Password',
-	'infractions_settings_updated_successfully' => 'Infractions settings have been updated successfully.',
-	'unable_to_write_infractions_config' => 'Unable to write to file <strong>modules/infractions/config.php</core>. Please check file permissions.',
+    'infractions_settings' => 'Infractions Settings',
+    'infractions_view' => 'View Infractions',
+    'link_location' => 'Link Location',
+    'plugin' => 'Plugin Integration',
+    'database_settings' => 'Update Database Settings',
+    'database_address' => 'Database Address',
+    'database_name' => 'Database Name',
+    'database_username' => 'Database Username',
+    'database_port' => 'Database Port',
+    'database_password' => 'Database Password',
+    'infractions_settings_updated_successfully' => 'Infractions settings have been updated successfully.',
+    'unable_to_write_infractions_config' => 'Unable to write to file <strong>modules/infractions/config.php</core>. Please check file permissions.',
 );
